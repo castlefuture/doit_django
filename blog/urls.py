7cf913cc9dded531b167150ca51aed82ce3e50d8
mirae <castlefuture12@gmail.com>
2022-10-26 18:14:53 +0900

    CBV로 블로그 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from blog.views import index
+# from blog.views import index
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()), # CBV 방식의 call
+    # path('', views.index), # FBV 방식의 call
+    # path('<int:pk>/', views.single_post_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
+
+
+class PostList(ListView):
+  model = Post
+  template_name = 'blog/index.html'
+
+class PostDetail(DetailView):
+  model = Post
+
 # 샘플
 # def index(request):
 #     # 필요한 코드 넣기
@@ -9,24 +19,27 @@ from .models import Post
 #     # render(request, '템플릿파일', {'키':변수값})
 #     return render(request, 'blog/index.html', {'datas' : datas})
 
-def index(request):
-  posts = Post.objects.all().order_by('-pk')
+# blog list up
+# def index(request):
+#   posts = Post.objects.all().order_by('-pk')
 
-  return render(
-    request,
-    'blog/index.html',
-    {
-        'posts': posts,
-    }
-  )
+#   return render(
+#     request,
+#     'blog/index.html',
+#     {
+#         'posts': posts,
+#     }
+#   )
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
 
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+# 하나의 post 상세 페이지
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post': post,
+#         }
+#     )
